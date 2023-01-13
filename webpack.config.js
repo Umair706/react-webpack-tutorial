acab93ff1aa5c80636e618881fe05bc351b59134
Muhammad Umair <muhammad.umair@safarifoneict.com>
2023-01-13 13:57:52 +0500

    Learn Webpack: Article II

--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,6 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
-      {
-        test: /\.(png|svg|jpeg|jpg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env",["@babel/preset-react",{"runtime":"automatic"}]]
-          },
-        },
-      },
     ],
   },
   plugins: [
